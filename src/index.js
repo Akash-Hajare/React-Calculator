708df01fdfaf40c6042aace57570026a28595358
Akash-Hajare <118100813+Akash-Hajare@users.noreply.github.com>
2023-08-14 18:33:40 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './style.css'; // Import the CSS file
+
+//ReactDOM.render()
+ReactDOM.render( <App /> ,  document.getElementById("root"))
